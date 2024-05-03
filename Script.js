3fe68d30a0437e0aa2d76a8e7902f5d4dff3872a
OnStarPrograms <on@starprograms.dev>
2024-05-02 20:49:46 -0400

    aba

--- Script.js
+++ Script.js
@@ -2,8 +2,15 @@ function Fetched(json)
 {
     console.log(json.Information[0].head);
     console.log(json.Projects[0].Project_Name);
+    for (let i = 0; i < json.Information.length; i++)
+    {
+        document.body.innerHTML += "<h1>"+json.Information[i].head+"</h1>";
+    }
 }
 
-fetch('.Data/Data.json') //.Data/Data.json
+
+//https://test.starprograms.dev/Data/Data.json - WebJson
+//.Data/Data.json - Local Json
+fetch('https://test.starprograms.dev/Data/Data.json') 
     .then((response) => response.json())
     .then((json) => Fetched(json));
