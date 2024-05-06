df4c971d3ea90d0c3ebce78a52fa4cd3833afb5e
OnStarPrograms <on@starprograms.dev>
2024-05-06 19:42:09 -0400

    bruh update the Data JSON

--- Script.js
+++ Script.js
@@ -14,16 +14,7 @@ function loadData()
     document.getElementById("Text").innerHTML = "";
     document.getElementById("Text").innerHTML = jsonner.Information[Math.abs(track)].Data.Data_Text;
     var image = jsonner.Information[Math.abs(track)].Data.image;
-    if (image[0] == true)
-    {
-        if (image[1] == "center")
-        {
-            for (var i = 2; i < image.length; i++)
-            {
-                document.getElementById("Text").innerHTML += '<img id = "Image" src = "'+image[i]+'" alt = "MaxWell">';
-            }
-        }
-    }
+    load_images(image);
 }
 
 function Fetched()
@@ -50,6 +41,19 @@ function Fetched()
     }
     choices(MChoice);
 }
+
+function load_images(imageList) {
+    if (imageList[0] == true)
+        {
+            if (imageList[1] == "center")
+            {
+                for (var i = 2; i < imageList.length; i++)
+                {
+                    document.getElementById("Text").innerHTML += '<img id = "Image" src = "'+imageList[i]+'" alt = "MaxWell">';
+                }
+            }
+        }
+  }
 
 function TogglePage()
 {
