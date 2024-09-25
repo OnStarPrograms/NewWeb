905a996445fbbd0a55fdf4fd924843d5ca4371ca
OnStarPrograms <aidenthomas711@gmail.com>
2024-09-25 13:22:46 -0400

    Fixed Mobile CSS

--- Script.js
+++ Script.js
@@ -35,7 +35,7 @@ function loadData()
         for (var i = 0; i < projects.length; i++)
         {
             var current_Project = jsonner.Projects[projects[i]];
-            document.getElementById("Text").innerHTML += "<h4 style = 'font-size: 2em; text-decoration: underline;'>"+current_Project.Project_Name+"<h4>";
+            document.getElementById("Text").innerHTML += "<h4 id = 'ProgHead' style = 'text-decoration: underline;'>"+current_Project.Project_Name+"<h4>";
             load_links(current_Project.Links);
             var mset = 0;
             if (current_Project.image[0] == true)
@@ -47,7 +47,7 @@ function loadData()
                 }
             }
             for (var j = 0; j < current_Project.Project_info.length; j++)
-                document.getElementById("Text").innerHTML += "<h4 style = 'font-size: 1em;'>"+current_Project.Project_info[j]+"<h4>";
+                document.getElementById("Text").innerHTML += "<h4 id='ProgBody'>"+current_Project.Project_info[j]+"<h4>";
             if (mset == 0)
             {
                 load_images(current_Project.image);
